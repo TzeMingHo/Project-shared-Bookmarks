54c38d9a3dc3e28e22e2f3c0c01f86e32d344da0
Ahmad Hmedan <ahmadhmedann@gmail.com>
2026-02-11 14:41:42 +0000

    feat:  sort bookmarks by created timestamp (nweset first)

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const allBookmarks = [
     title: "Type Club",
     url: "https://www.typingclub.com/",
     description: "This is to improve your typing speed.",
-    timestamp: "2026-02-08T10:30:00Z",
+    timestamp: "2026-02-09T10:29:00Z",
     likeCounter: 0,
     bookmarkId: 2,
   },
@@ -41,7 +41,8 @@ import { getUserIds } from "./storage.js";
 function displayingBookmarks() {
    const displayArea = document.getElementById("bookmark-display-area");
    displayArea.textContent = ""; // to clear the display before rendering
-  const bookmarks = allBookmarks.map(bookmarkCard);
+   const sortByNewestFirst = [...allBookmarks].sort((a,b)=> Date.parse(b.timestamp) - Date.parse(a.timestamp));
+  const bookmarks = sortByNewestFirst.map(bookmarkCard);
   displayArea.append(...bookmarks);
 }
 
